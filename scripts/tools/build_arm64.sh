ee64e263e7997b34411ba3d0ab5046d0568b5077
Sameer Srivastava <itsmesamster@gmail.com>
2023-12-12 23:32:34 +0100

    Test commit 26

---
+++ scripts/tools/build_arm64.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eux
+uname -a
+sudo apt-get install -y libudev-dev
+sudo apt-get install ruby-full
+sudo apt-get install nodejs
+curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
+gem install dotenv
+gem install json
+cargo install nj-cli
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+cd ./application/holder
+rake release:prod --trace
