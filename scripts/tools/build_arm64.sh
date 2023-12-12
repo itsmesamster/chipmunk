61de3b6337bce0c7f8ed8e8c49c644acf1d759c3
Sameer Srivastava <itsmesamster@gmail.com>
2023-12-12 23:54:47 +0100

    Test commit 31

--- scripts/tools/build_arm64.sh
+++ scripts/tools/build_arm64.sh
@@ -3,12 +3,12 @@ set -eux
 uname -a
 sudo apt-get install -y libudev-dev
 # sudo apt-get install ruby-full
-sudo apt-get install nodejs
+sudo apt-get install -y nodejs
 curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 gem install dotenv
 gem install json
-cargo install nj-cli
+cargo install -y nj-cli
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 cd ./application/holder
 rake release:prod --trace
