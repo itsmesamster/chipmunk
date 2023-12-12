f8e266b0ff94178f44476af1783a88e87267777b
Sameer Srivastava <itsmesamster@gmail.com>
2023-12-12 23:37:58 +0100

    Test commit 28

--- scripts/tools/build_arm64.sh
+++ scripts/tools/build_arm64.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -eux
 uname -a
-sudo apt-get install -y libudev-dev
+apt-get install -y libudev-dev
 sudo apt-get install ruby-full
 sudo apt-get install nodejs
 curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
 gem install dotenv
 gem install json
 cargo install nj-cli
