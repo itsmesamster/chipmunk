124689f48ea21566127c3af8ec7179d910d2fab9
Sameer Srivastava <itsmesamster@gmail.com>
2023-12-13 00:44:46 +0100

    Test commit 38

--- scripts/tools/build_arm64.sh
+++ scripts/tools/build_arm64.sh
@@ -2,16 +2,16 @@
 set -eux
 uname -a
 # sudo apt-get update
-sudo apt-get install -y curl
-sudo apt-get install -y libudev-dev
-sudo apt-get update && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata
-sudo apt-get install -y ruby-full
-sudo apt-get install -y nodejs
-curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
-gem install dotenv
-gem install json
-cargo install -y nj-cli
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-cd ./application/holder
-rake release:prod --trace
+# sudo apt-get install -y git curl autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
+# sudo apt-get install -y libudev-dev
+# sudo apt-get update && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata
+# sudo apt-get install -y ruby-full
+# sudo apt-get install -y nodejs
+# curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y
+# source $HOME/.cargo/env
+# gem install dotenv
+# gem install json
+# cargo install -y nj-cli
+# curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+# cd ./application/holder
+# rake release:prod --trace
